40c5407e810619669f6ce3d4e6218ead35c37108
ViktorBukarev <bukarev1985@gmail.com>
2022-02-18 01:29:45 +0300

    Test2

--- UI/conftest.py
+++ UI/conftest.py
@@ -5,13 +5,10 @@ from selenium.webdriver.chrome.options import Options
 
 #Указываем предварительные настройки
 
-
-
-
 @pytest.fixture(scope="session")
 def browser():
     options = Options()
     options.add_argument("window-size=1920,1080")
-    driver = webdriver.Chrome(chrome_options=options)
+    driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
